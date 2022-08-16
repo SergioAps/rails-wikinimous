3f960bdc6b3f7ada541aee982f57bbf1a65219aa
Eduardo Aguilar <laloaps@gmail.com>
2022-08-16 15:46:22 -0400

    destroy article

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -29,6 +29,14 @@ class ArticlesController < ApplicationController
     redirect_to article_path(@article)
   end
 
+  def destroy
+    @article = Article.find(params[:id])
+    @article.destroy
+
+    redirect_to article_path(@article)
+  end
+
+
   private
 
   def article_params
